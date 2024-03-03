81458d3c37e61f66c7ef00bdd68dfe44f171093d
STPR2001 <santiagoperron2016@gmail.com>
2024-03-03 02:17:58 -0300

    app was deploy

---
+++ public/scripts/index.js
@@ -0,0 +1,21 @@
+import {Contador} from "./contador.js"
+
+
+window.alert("No vale llorar");
+
+
+const p1 = new Contador("Nosotros", document.getElementById("jugador1Container"));
+const p2 = new Contador("Ellos", document.getElementById("jugador2Container"));
+
+document.getElementById("reset").addEventListener("click",()=>{
+    p1.reset();
+    p2.reset();
+})
+
+p1.agregar();
+p1.reset();
+p1.agregar();
+p1.restar();
+p1.restar();
+p2.agregar();
+p2.restar();
